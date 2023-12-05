caa19d781016ff690d7a48b0cf38077f96672621
audlr <118803331+audlr@users.noreply.github.com>
2023-12-05 23:23:12 +0900

    Update app1.py

--- app1.py
+++ app1.py
@@ -47,48 +47,45 @@ if st.session_state.page_number == 2:
 
     next_button_1 = st.button("결과 확인", key="next_button_1")
     if next_button_1:
-        st.session_state.page_number += 1
+        container1.expander(f"Choice")
 
-if st.session_state.page_number == 3:
-    container1.expander(f"Choice")
-
-    st.write("---")
-    Answer_name = st.empty()
-    Answer_name.subheader(f"{user_name} 님께서 선택하신 곳과 비슷한 식당이에요")
-
-    recommended_df = pd.read_csv('Freshmenu_dataframe.csv',
-                 engine='python', on_bad_lines='skip', encoding='utf-8')
-
-    if recommended_df is not None:  # Check if recommended_df is not None
-        container2 = st.empty()
-        with container2.expander(f"Recommend", expanded=True):
-            for i in range(5):
-                col1, col2 = st.columns(2, gap="small")
-                with col1:
-                    st.write(f"식당: {recommended_df['name'].values[i]}")
-                    st.write(f"주소: {recommended_df['address'].values[i]}")
-                    st.write(f"cuisines: {recommended_df['cuisines'].values[i]}")
-                    review_text = recommended_df['reviews_list'].values[i]
-                    st.write(f"리뷰: {review_text[:500]}")
-                with col2:
-                    geolocator = Nominatim(user_agent="my_geocoder")
-                    location = geolocator.geocode(recommended_df['address'].values[i])
-
-                    if location:
-                        latitude, longitude = location.latitude, location.longitude
-                        recommended_df.loc[i, 'Latitude'] = latitude
-                        recommended_df.loc[i, 'Longitude'] = longitude
-
-                        m = folium.Map(location=[latitude, longitude], zoom_start=15)
-                        folium.Marker([latitude, longitude], popup=f"{recommended_df['address'].iloc[i]}").add_to(m)
-                        folium_static(m, width=500, height=400)
-                    else:
-                        st.warning(f"Location not found for {recommended_df['name'].values[i]}. Skipping map creation.")
-    else:
-        st.warning("Data not found. Please provide valid data.")
-
-
-    next_button_2 = st.button("다시 선택", key="next_button_2")
-    if next_button_2 and not selected_restaurant.empty:
-        st.session_state.page_number -= 1
-        Answer_name.empty()
+        st.write("---")
+        Answer_name = st.empty()
+        Answer_name.subheader(f"{user_name} 님께서 선택하신 곳과 비슷한 식당이에요")
+    
+        recommended_df = pd.read_csv('Freshmenu_dataframe.csv',
+                     engine='python', on_bad_lines='skip', encoding='utf-8')
+    
+        if recommended_df is not None:  # Check if recommended_df is not None
+            container2 = st.empty()
+            with container2.expander(f"Recommend", expanded=True):
+                for i in range(5):
+                    col1, col2 = st.columns(2, gap="small")
+                    with col1:
+                        st.write(f"식당: {recommended_df['name'].values[i]}")
+                        st.write(f"주소: {recommended_df['address'].values[i]}")
+                        st.write(f"cuisines: {recommended_df['cuisines'].values[i]}")
+                        review_text = recommended_df['reviews_list'].values[i]
+                        st.write(f"리뷰: {review_text[:500]}")
+                    with col2:
+                        geolocator = Nominatim(user_agent="my_geocoder")
+                        location = geolocator.geocode(recommended_df['address'].values[i])
+    
+                        if location:
+                            latitude, longitude = location.latitude, location.longitude
+                            recommended_df.loc[i, 'Latitude'] = latitude
+                            recommended_df.loc[i, 'Longitude'] = longitude
+    
+                            m = folium.Map(location=[latitude, longitude], zoom_start=15)
+                            folium.Marker([latitude, longitude], popup=f"{recommended_df['address'].iloc[i]}").add_to(m)
+                            folium_static(m, width=500, height=400)
+                        else:
+                            st.warning(f"Location not found for {recommended_df['name'].values[i]}. Skipping map creation.")
+        else:
+            st.warning("Data not found. Please provide valid data.")
+    
+    
+        next_button_2 = st.button("다시 선택", key="next_button_2")
+        if next_button_2 and not selected_restaurant.empty:
+            st.session_state.page_number -= 1
+            Answer_name.empty()
